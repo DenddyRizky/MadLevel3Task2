68ac1b2fad48a52e751d32328e472a3d6e0bb639
Denddy <denddyrizky@gmail.com>
2020-09-23 12:17:37 +0200

    Made the layout, did some refactoring of the names and added portal classes

---
+++ app/src/main/java/com/example/madlevel3task2/PortalAdapter.kt
@@ -0,0 +1,29 @@
+package com.example.madlevel3task2
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_portal.view.*
+
+class PortalAdapter(private val portals: List<Portal>) : RecyclerView.Adapter<PortalAdapter.ViewHolder>(){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PortalAdapter.ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_portal, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return portals.size
+    }
+
+    override fun onBindViewHolder(holder: PortalAdapter.ViewHolder, position: Int) {
+        holder.databind(portals[position])
+    }
+
+    inner class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(itemView){
+        fun databind(portal: Portal){
+            itemView.tvPortalUrl.text = portal.portalUrl
+        }
+    }
+}
